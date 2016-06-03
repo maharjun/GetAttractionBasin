f1e92e548482d7d03ee930fa13cae7de8e22283f
Arjun Rao <arjun210493@gmail.com>
2016-06-03 22:34:45 +0530

    Added SetupDynSystem.py
    Changes:
    
    1.  Updated CMakeModules to be able to use the CMake call python wra-
        pper functions
    
    2.  Added SetupDynSystem.py to provide the functionality of generator
        checking and other miscellania for setting up a particular dynamic
        system
    
    3.  Added __pycache__ to gitignore

---
+++ SetupDynSystem.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3.5
+
+import sys
+import os
+from RepoManagement.BasicUtils import changedDir, getFrameDir
+
+
+def SetupDynSystem(DynSystemHPPPath=None):
+
+    if not DynSystemHPPPath:
+        DynSystemHPPPath = ''
+
+    # get Current Script Directory
+    CurrentScriptDir = getFrameDir()
+
+    # Initialize Directories relevant to CMake
+    CMakeModulesDir = os.path.join(CurrentScriptDir, 'CMakeModules')
+    CurrentSourceDir = CurrentScriptDir
+    BuildDir = os.path.join(os.getcwd(), 'build')
+
+    # Add CMakeModulesDir to sys path to get relevant functions
+    sys.path.insert(0, CMakeModulesDir)
+    from CMakePyHelper import getDefaultPlatformGen
+    from CMakePyHelper import CMakeGenCall, CMakeBuildCall
+
+    DefaultGenerator = getDefaultPlatformGen()
+
+    # Initialize Path to the HPP file correspondingto the dynamic system.
+    DynSystemHPPPath = os.path.join(os.getcwd(), DynSystemHPPPath)
+    
+    # generate and build
+    if not os.path.isdir(BuildDir):
+        os.mkdir(BuildDir)
+    with changedDir(BuildDir):
+        isCMakeGenSuccess = CMakeGenCall(CurrentSourceDir,
+                                         Generator=DefaultGenerator,
+                                         BuildConfig='Release',
+                                         Silent=True,
+                                         DYN_SYSTEM_HPP_PATH=DynSystemHPPPath)
+    if isCMakeGenSuccess:
+        CMakeBuildSuccess = CMakeBuildCall(BuildDir,
+                                           Target='install',
+                                           BuildConfig='Release')
+
+    return CMakeBuildSuccess
+
+
+if __name__ == "__main__":
+    argc = len(sys.argv)
+    argv = sys.argv
+
+    if argc == 1:
+        DynSystemHPPPath = None
+    elif argc == 2:
+        DynSystemHPPPath = argv[1]
+    else:
+        print("Incorrect number of arguments expected at max 1 argument")
+
+    SetupDynSystem(DynSystemHPPPath)
